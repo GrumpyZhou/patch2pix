bcbc0a034cd956f8d76d8fb014cdd7a8597efc36
Qunjie Zhou <jennyzqj00@gmail.com>
2022-06-04 22:02:24 +0200

    Remove outdated link (#11)

--- data/prepare_immatch_val_data.sh
+++ data/prepare_immatch_val_data.sh
@@ -1,4 +1,6 @@
 # Download and extracting validation set
+# Dataset link: https://www.cs.ubc.ca/~kmyi/imw2020/data.html
+
 mkdir immatch_benchmark
 mkdir immatch_benchmark/val_dense
 cd immatch_benchmark/val_dense
@@ -14,9 +16,6 @@ tar -xvzf st_peters_square.tar.gz
 
 wget https://www.cs.ubc.ca/research/kmyi_data/imw2020/TrainingData/taj_mahal.tar.gz
 tar -xvzf taj_mahal.tar.gz
-
-wget https://www.cs.ubc.ca/research/kmyi_data/imw2020/TrainingData/hagia_sophia_interior.tar.gz
-tar -xvzf hagia_sophia_interior.tar.gz
 
 wget https://www.cs.ubc.ca/research/kmyi_data/imw2020/TrainingData/temple_nara_japan.tar.gz
 tar -xvzf temple_nara_japan.tar.gz
