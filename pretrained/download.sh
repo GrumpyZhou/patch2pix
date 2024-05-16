0b3e0fcf7b4766927af58473bef260ae7cee513f
Qunjie Zhou <jennyzqj00@gmail.com>
2024-05-16 17:26:15 +0200

    Migrate pretrained models  (#15)

--- pretrained/download.sh
+++ pretrained/download.sh
@@ -1,2 +1,5 @@
-wget https://vision.in.tum.de/webshare/u/zhouq/patch2pix/pretrained/patch2pix_pretrained.pth
-wget https://vision.in.tum.de/webshare/u/zhouq/patch2pix/pretrained/ncn_ivd_5ep.pth
+# Patch2pix pretrained - https://drive.google.com/file/d/1ZbJIE0LcZ3Oti-h8zU72JRL7LryVxJOh/view?usp=drive_link
+gdown 1ZbJIE0LcZ3Oti-h8zU72JRL7LryVxJOh
+
+# NCNet IVD  - https://drive.google.com/file/d/10GZ0x3CmObKzbAg1GKQhrkPeSLRpD4Rp/view?usp=drive_link
+gdown 10GZ0x3CmObKzbAg1GKQhrkPeSLRpD4Rp
